e7374e72e628aee1ed4be31fbf00336fa410b687
ant <anthonypapasavva@gmail.com>
2022-09-01 15:05:28 +0100

    add dev branch, again

---
+++ bin/src/main/resources/data-dev.sql
@@ -0,0 +1,4 @@
+INSERT INTO booking(`booker_name`, `movie_date`, `movie_title`, `number_of_seats`, `ticket_type`, `total`) 
+VALUES 
+('John Smith', '2020-05-03', 'Ghostbusters', 2, 'adult', 20),
+('Piers Barber', '2021-05-03', 'Ghostbusters 2', 1, 'adult', 10);
