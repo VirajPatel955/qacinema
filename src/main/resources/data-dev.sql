1c5ed9bdbd3d6ef30d2c9159bc27d19d560df414
Ian Petts <itisianpetts@gmail.com>
2022-09-01 12:00:30 +0100

    sql update

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -9,5 +9,5 @@ create table booking (
     total bigint not null,
     primary key (id));
 
-INSERT INTO booking (booker_name, movie_date, movie_title, number_of_seats, ticket_type, total) values ("John Smith", 2020-05-03, "Ghostbusters", 2, "adult", 20);
+INSERT INTO booking (booker_name, movie_date, movie_title, number_of_seats, ticket_type, total) values ('John Smith', 2020-05-03, 'Ghostbusters', 2, 'adult', 20);
 INSERT INTO booking (booker_name, movie_date, movie_title, number_of_seats, ticket_type, total) values ("Bob the Builder", 2021-05-03, "Ghostbusters 2", 1, "adult", 10);
