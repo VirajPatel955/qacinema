525d52626e144fb0efca4b11972c88fe2e6eef16
feiza15 <feizaahmed159@gmail.com>
2022-09-01 11:30:58 +0100

    sql

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -10,4 +10,5 @@ create table booking (
     total bigint not null,
     primary key (id));
 
-INSERT INTO qacinemaDB.booking (booker_name, date, movie_title, number_of_seats, ticket_type, time, total) values ("John Smith", 2020/05/03, "Ghostbusters", 2, "adult", '2020-05-03 11:11:11.11111', 20);
+INSERT INTO booking (booker_name, date, movie_title, number_of_seats, ticket_type, time, total) values ("John Smith", 2020-05-03, "Ghostbusters", 2, "adult", '2020-05-03 11:11:11.11111', 20);
+INSERT INTO qacinemaDB.booking (booker_name, date, movie_title, number_of_seats, ticket_type, time, total) values ("Bob the Builder", 2021-05-03, "Ghostbusters 2", 1, "adult", '2020-05-03 11:11:11.11111', 10);
