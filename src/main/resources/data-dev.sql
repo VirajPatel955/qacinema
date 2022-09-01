200657abf8cfb5e1bb66a2f76d90b2c462a78032
ant <anthonypapasavva@gmail.com>
2022-09-01 12:39:49 +0100

    sql syntax update

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -14,5 +14,5 @@ create table if not exists `booking` (
     
 INSERT INTO booking(`booker_name`, `movie_date`, `movie_title`, `number_of_seats`, `ticket_type`, `total`) 
 VALUES 
-(`John Smith`, 2020-05-03, `Ghostbusters`, 2, `adult`, 20),
-(`Bob the Builder`, 2021-05-03, `Ghostbusters 2`, 1, `adult`, 10);
+('John Smith', '2020-05-03', 'Ghostbusters', 2, 'adult', 20),
+('Bob the Builder', '2021-05-03', 'Ghostbusters 2', 1, 'adult', 10);
