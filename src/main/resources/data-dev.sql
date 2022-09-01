4e2fc9016274f534565d01027a519e84b7eb1a5b
feiza15 <feizaahmed159@gmail.com>
2022-09-01 11:37:48 +0100

    sql stuff

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -2,13 +2,12 @@ drop table if exists booking
 create table booking (
     id bigint not null auto_increment,
     booker_name varchar(15) not null,
-    date date not null,
+    movieDate date not null,
     movie_title varchar(15) not null,
     number_of_seats bigint not null,
     ticket_type varchar(15) not null,
-    time datetime(6) not null,
     total bigint not null,
     primary key (id));
 
-INSERT INTO booking (booker_name, date, movie_title, number_of_seats, ticket_type, time, total) values ("John Smith", 2020-05-03, "Ghostbusters", 2, "adult", '2020-05-03 11:11:11.11111', 20);
-INSERT INTO qacinemaDB.booking (booker_name, date, movie_title, number_of_seats, ticket_type, time, total) values ("Bob the Builder", 2021-05-03, "Ghostbusters 2", 1, "adult", '2020-05-03 11:11:11.11111', 10);
+INSERT INTO qacinemaDB.booking (booker_name, movieDate, movie_title, number_of_seats, ticket_type, total) values ("John Smith", 2020-05-03, "Ghostbusters", 2, "adult", 20);
+INSERT INTO qacinemaDB.booking (booker_name, movieDate, movie_title, number_of_seats, ticket_type, total) values ("Bob the Builder", 2021-05-03, "Ghostbusters 2", 1, "adult", 10);
