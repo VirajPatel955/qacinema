809441f23e7ce709a5aa7b86211a2ffdb11debdb
ant <anthonypapasavva@gmail.com>
2022-09-01 11:56:18 +0100

    sql syntax update

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -2,12 +2,12 @@ drop table if exists booking
 create table booking (
     id bigint not null auto_increment,
     booker_name varchar(15) not null,
-    movieDate date not null,
+    movie_date date not null,
     movie_title varchar(15) not null,
     number_of_seats bigint not null,
     ticket_type varchar(15) not null,
     total bigint not null,
     primary key (id));
 
-INSERT INTO qacinemaDB.booking (booker_name, movieDate, movie_title, number_of_seats, ticket_type, total) values ("John Smith", 2020-05-03, "Ghostbusters", 2, "adult", 20);
-INSERT INTO qacinemaDB.booking (booker_name, movieDate, movie_title, number_of_seats, ticket_type, total) values ("Bob the Builder", 2021-05-03, "Ghostbusters 2", 1, "adult", 10);
+INSERT INTO booking (booker_name, movie_date, movie_title, number_of_seats, ticket_type, total) values ("John Smith", 2020-05-03, "Ghostbusters", 2, "adult", 20);
+INSERT INTO booking (booker_name, movie_date, movie_title, number_of_seats, ticket_type, total) values ("Bob the Builder", 2021-05-03, "Ghostbusters 2", 1, "adult", 10);
