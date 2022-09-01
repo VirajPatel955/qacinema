f634e07e995e337d0ad0e3bbf830e4e264de535a
feiza15 <feizaahmed159@gmail.com>
2022-09-01 16:41:04 +0100

    chnaged the column name in the sql file

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -3,6 +3,6 @@ VALUES
 ('John Smith', '2020-05-03', 'Ghostbusters', 2, 'adult', 20),
 ('Piers Barber', '2021-05-03', 'Ghostbusters 2', 1, 'adult', 10);
 
-INSERT INTO listing(`movie_title`, `director`, `actors`, `showing_times`)
+INSERT INTO listing(`movie`, `director`, `actors`, `showing_times`)
 VALUES
     ('Ghostbusters', 'John Abrahams', 'Paul Rudd', 1700);
